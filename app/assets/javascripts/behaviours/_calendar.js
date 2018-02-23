d575c243517b4f1044d29aca853fdbc8ef432db3
clouie87 <carol.louie@gmail.com>
2018-02-22 23:13:42 -0800

    Puts Calendar and Map in same section
    - allows users to click on events and it will populate the map to show location.
    - Still need to do some work on the js of calendar.

--- app/assets/javascripts/behaviours/_calendar.js
+++ app/assets/javascripts/behaviours/_calendar.js
@@ -5,20 +5,27 @@ function eventCalendar() {
   $('#event_calendar').fullCalendar({
     header: {
     center: '',
-    right:  'today prev,next'
-},
+    right:  'today prev, next'
+    },
     displayEventEnd: true,
     eventColor: '#378006',
     events: '/events.json',
     timeFormat: 'h(:mm)',
-    eventRender: function(event, element) {
-      $(element).append("<span class='calendar-description'>"+ event.address+"</span>")
-    },
     eventClick: function(calEvent, jsEvent, view) {
-       $(this).css('border-color', 'red');
-       // create a modal
-       // eventModal(calEvent);
-   }
+      $(this).css('border-color', 'red');
+      var home = {lat: 49.2897713, lng: -123.1343084};
+
+      var map = new google.maps.Map(document.getElementById('google-map'), {
+        zoom: 12,
+        center: home
+      });
+
+      return new google.maps.Marker({
+        position: { lat: calEvent.lat, lng: calEvent.lng },
+        label: "A",
+        map: map
+      });
+    }
   });
 };
 
--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,19 +1,19 @@
-function initMap() {
+function initMap(events = []) {
   var results = [];
   var new_data = [];
   var home = {lat: 49.2897713, lng: -123.1343084};
 
+
   var map = new google.maps.Map(document.getElementById('google-map'), {
-    zoom: 16,
+    zoom: 12,
     center: home
   });
 
   var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-
   $.ajax({
     dataType: "json",
-    url: 'events/todays_events.json',
+    url: 'events.json',
     success: function(data) {
       $(data).map(function() {
         if(this.lat != null && this.lng != null) {
@@ -23,6 +23,7 @@ function initMap() {
       });
 
       var markers = results.map(function(location, i) {
+
         return new google.maps.Marker({
           position: location,
           label: labels[i % labels.length],
@@ -33,4 +34,11 @@ function initMap() {
       return markers
     }
   })
+}
+
+function getMap() {
+  var home = {lat: 49.2897713, lng: -123.1343084};
+  return new google.maps.Map(document.getElementById('google-map'), {
+    zoom: 16,
+  });
 }
