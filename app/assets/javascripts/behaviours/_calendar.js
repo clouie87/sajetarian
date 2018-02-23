6ca2469697b6dd26917d2f5d8a26401b3ac94d20
clouie87 <carol.louie@gmail.com>
2018-02-22 23:29:08 -0800

    Center map to marker
    - do not hard set center position. center the map to the marker.

--- app/assets/javascripts/behaviours/_calendar.js
+++ app/assets/javascripts/behaviours/_calendar.js
@@ -13,18 +13,18 @@ function eventCalendar() {
     timeFormat: 'h(:mm)',
     eventClick: function(calEvent, jsEvent, view) {
       $(this).css('border-color', 'red');
-      var home = {lat: 49.2897713, lng: -123.1343084};
 
       var map = new google.maps.Map(document.getElementById('google-map'), {
         zoom: 12,
-        center: home
       });
 
-      return new google.maps.Marker({
+      var marker = new google.maps.Marker({
         position: { lat: calEvent.lat, lng: calEvent.lng },
         label: "A",
         map: map
       });
+
+      return map.panTo(marker.getPosition());
     }
   });
 };
