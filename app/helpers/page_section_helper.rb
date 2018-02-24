dba302e3f7c530ed661702363eb4a3b90e7ed1e3
clouie87 <carol.louie@gmail.com>
2018-02-23 18:28:16 -0800

    Parse Content
    - fixes the way content appears. Gives correct breaks that have been created in the backend.

--- app/helpers/page_section_helper.rb
+++ app/helpers/page_section_helper.rb
@@ -3,6 +3,13 @@ module PageSectionHelper
     section.page_type.name.downcase
   end
 
+  def parsed_content(content)
+    paragraphs = content.split(/\n+/)
+    paragraphs.map do |paragraph|
+      content_tag :p, paragraph
+    end.join.html_safe
+  end
+
   def menu_item_categories
     MenuItem.all.roots
   end
