0764bc25f11f28afbaafd9c9e8510dc62f0e7813
clouie87 <carol.louie@gmail.com>
2018-02-23 19:10:05 -0800

    Makes fixed header
    - sets the header to be fixed
    - makes the scroll a little faster

--- app/assets/javascripts/behaviours/slow-scroll.js
+++ app/assets/javascripts/behaviours/slow-scroll.js
@@ -1,6 +1,8 @@
 $(document).on('click', '[data-behaviour~="scroll-button"]', function(event) {
   var target = $(event.target).data("target");
+  var headerHeight = $(".nav-container").height();
+
   $('html, main').animate({
-    scrollTop: $("#" + target).offset().top
-  }, 2000);
+    scrollTop: $("#" + target).offset().top - headerHeight
+  }, 1600);
 })
