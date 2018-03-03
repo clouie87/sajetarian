000d042332184441f5e34d7558e59796a0202458
clouie87 <carol.louie@gmail.com>
2018-03-03 15:46:03 -0800

    Setting up Email Server for Sajetarian
    - configure email settings in sajetarian

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,14 +57,12 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sajetarian_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = {:host => "www.sajetarian.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                  587,
-    domain:               'sajetarian.com',
-    user_name:            '<username>',
-    password:             '<password>',
-    authentication:       'plain',
+    domain:               '198.199.73.208',
     enable_starttls_auto: true  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
