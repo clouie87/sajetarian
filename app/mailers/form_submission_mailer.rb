7858b8e2fff3cbbb2626d381b93d3137523088b9
clouie87 <carol.louie@gmail.com>
2018-03-03 17:08:42 -0800

    Fixes Mailers
    - needed to create a template for the mailer.
    - updated the information in the email that we send out.
    - inherit the base application mailer (so dont need to define layout/default email).
    - create test preview. Allowed me to debug this email template issue.

--- app/mailers/form_submission_mailer.rb
+++ app/mailers/form_submission_mailer.rb
@@ -1,9 +1,6 @@
 class FormSubmissionMailer < ApplicationMailer
-  layout 'mailer'
-
-  default from: 'adarwish@sajetarian.com'
-
   def thank_you_message(user)
+    @user = user
     mail(to: user.email, subject: 'Thank you for contacting Sajetarian')
   end
 end
--- test/mailers/previews/form_submission_mailer_preview.rb
+++ test/mailers/previews/form_submission_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/form_submission_mailer
 class FormSubmissionMailerPreview < ActionMailer::Preview
-
+  def thank_you_message
+    FormSubmissionMailer.thank_you_message(FormSubmission.first)
+  end
 end
