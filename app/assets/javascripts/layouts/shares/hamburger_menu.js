58867b3899c1faaba95648c93c20884b429d3666
clouie87 <carol.louie@gmail.com>
2018-03-10 22:00:33 -0800

    Fixes click on mobile
    - makes click work on mobile.

--- app/assets/javascripts/layouts/shares/hamburger_menu.js
+++ app/assets/javascripts/layouts/shares/hamburger_menu.js
@@ -2,8 +2,8 @@ $(function() {
   showMenuButton = '[data-behaviour~=show-hamburger-menu]';
   closeMenuButton = '[data-behaviour~=hide-hamburger-menu]';
 
-  $(document).on('click', showMenuButton, openHambugerMenu);
-  $(document).on('click', closeMenuButton, closeHambugerMenu);
+  $(document).on('click touchstart', showMenuButton, openHambugerMenu);
+  $(document).on('click touchstart', closeMenuButton, closeHambugerMenu);
 
   function openHambugerMenu() {
     $(showMenuButton).hide();
