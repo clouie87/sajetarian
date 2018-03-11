30d589e57f71f2e4c9eb42f24da932f876ee22e7
clouie87 <carol.louie@gmail.com>
2018-03-10 22:27:27 -0800

    Fixing padding in contact form

--- app/assets/javascripts/behaviours/slow-scroll.js
+++ app/assets/javascripts/behaviours/slow-scroll.js
@@ -1,9 +1,10 @@
 $(document).on('click touchstart', '[data-behaviour~="scroll-button"]', function(event) {
   var target = $(event.target).data("target");
   var headerHeight = $(".nav-container").height();
-  $('[data-behaviour~=hide-hamburger-menu]').trigger('click');
 
   $('html, main').animate({
     scrollTop: $("#" + target).offset().top - headerHeight
   }, 1600);
+
+  $('[data-behaviour~=hide-hamburger-menu]').trigger('click');
 })
