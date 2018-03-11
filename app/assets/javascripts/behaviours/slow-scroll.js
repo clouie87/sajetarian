3e0f98c8c4c2ffac2a454f1f276c35dd6ca347b4
clouie87 <carol.louie@gmail.com>
2018-03-10 22:11:09 -0800

    Fixing mobile menu

--- app/assets/javascripts/behaviours/slow-scroll.js
+++ app/assets/javascripts/behaviours/slow-scroll.js
@@ -1,6 +1,7 @@
-$(document).on('click', '[data-behaviour~="scroll-button"]', function(event) {
+$(document).on('click touchstart', '[data-behaviour~="scroll-button"]', function(event) {
   var target = $(event.target).data("target");
   var headerHeight = $(".nav-container").height();
+  $('[data-behaviour~=hide-hamburger-menu]').trigger('click');
 
   $('html, main').animate({
     scrollTop: $("#" + target).offset().top - headerHeight
