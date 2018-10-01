16e1b7b72af43743c91e93a4382a36cf09d4a897
clouie87 <carol.louie@gmail.com>
2018-09-30 17:41:03 -0700

    Adds name to event
    - allows an event to be given a name external of the address.

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,8 +1,9 @@
 ActiveAdmin.register Event do
-  permit_params :starts_at, :ends_at, :address, :city, :latitude, :longitude, :full_street_address
+  permit_params :starts_at, :ends_at, :address, :city, :latitude, :longitude, :full_street_address, :name
 
   show do |t|
   		attributes_table do
+        row :name
   			row :starts_at
         row :ends_at
   			row :address
@@ -12,6 +13,7 @@ ActiveAdmin.register Event do
 
 	form :html => { :enctype => "multipart/form-data" } do |f|
 		f.inputs do
+      f.input :name
       f.input :starts_at, as: :datetime_picker
 			f.input :ends_at, as: :datetime_picker
 			f.input :address
